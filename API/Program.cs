bf4f4a5ad598ba91212f95a777988559fa747ad8
mubashir <mubashirsoomro60@gmail.com>
2023-12-12 21:24:12 +0500

    disable ssl

--- API/Program.cs
+++ API/Program.cs
@@ -37,7 +37,7 @@ else
         //pgHost = pgHost.Split(".")[0];
         var pgPort = pgHostPort.Split(":")[1];
 
-        connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+        connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL=false;SslMode=Disable";
 }
 builder.Services.AddDbContext<DataContext>(opt =>
 {
