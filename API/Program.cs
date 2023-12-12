bd7ab739cfcb12d5a41501b9ab520a4f0de9ee8e
mubashir <mubashirsoomro60@gmail.com>
2023-12-12 21:41:51 +0500

    updated to latest constring

--- API/Program.cs
+++ API/Program.cs
@@ -17,31 +17,30 @@ builder.Services.AddApplicationServices(builder.Configuration); // using extensi
 builder.Services.AddIdentityServices(builder.Configuration); // using extension methods 
 
 var connString = "";
-if (builder.Environment.IsDevelopment()) 
+if (builder.Environment.IsDevelopment())
     connString = builder.Configuration.GetConnectionString("DefaultConnection");
-else 
+else
 {
-// Use connection string provided at runtime by FlyIO.
-        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+    // Use connection string provided at runtime by FlyIO.
+    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-        // Parse connection URL to connection string for Npgsql
-        connUrl = connUrl.Replace("postgres://", string.Empty);
-        var pgUserPass = connUrl.Split("@")[0];
-        var pgHostPortDb = connUrl.Split("@")[1];
-        var pgHostPort = pgHostPortDb.Split("/")[0];
-        var pgDb = pgHostPortDb.Split("/")[1];
-        pgDb = pgDb.Split("?")[0];
-        var pgUser = pgUserPass.Split(":")[0];
-        var pgPass = pgUserPass.Split(":")[1];
-        var pgHost = pgHostPort.Split(":")[0];
-        //pgHost = pgHost.Split(".")[0];
-        var pgPort = pgHostPort.Split(":")[1];
+    // Parse connection URL to connection string for Npgsql
+    connUrl = connUrl.Replace("postgres://", string.Empty);
+    var pgUserPass = connUrl.Split("@")[0];
+    var pgHostPortDb = connUrl.Split("@")[1];
+    var pgHostPort = pgHostPortDb.Split("/")[0];
+    var pgDb = pgHostPortDb.Split("/")[1];
+    var pgUser = pgUserPass.Split(":")[0];
+    var pgPass = pgUserPass.Split(":")[1];
+    var pgHost = pgHostPort.Split(":")[0];
+    var pgPort = pgHostPort.Split(":")[1];
+    var updatedHost = pgHost.Replace("flycast", "internal");
 
-        connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SslMode=Disable";
+    connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
 }
 builder.Services.AddDbContext<DataContext>(opt =>
 {
-    opt.UseNpgsql(connString, optionsProvider => optionsProvider.EnableRetryOnFailure());
+    opt.UseNpgsql(connString);
 });
 
 var app = builder.Build();
